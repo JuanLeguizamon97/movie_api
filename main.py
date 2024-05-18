44ac761005c6c17fdd35f4941058e31de8c2bfba
JuanL <juanc.leguizamon@urosario.edu.co>
2024-05-17 21:00:37 -0500

    changing title, tags and versíon for the api

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title ="my FastAPI app"
+app.version = "0.0.1"
 
-@app.get('/')
+@app.get('/', tags=["home"])
 def message():
     return "Hello world!"
